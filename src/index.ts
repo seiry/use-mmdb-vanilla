1fdbe0fd3105f32092de7bf83abe6d80201df73e
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-03-27 13:53:57 +0800

    Update README.md with new package name and add CDN link for browser version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { getGeoFromIp } from "./getGeoFromIp";
 
+export { getGeoFromIp };
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).getGeoFromIp = getGeoFromIp;
