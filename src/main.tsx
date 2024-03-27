e050b46a186df2b277fe7d0888bd96c6ad0acddf
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-03-27 13:19:39 +0800

    Add getGeoFromIp import and display IP geolocation

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
+import { getGeoFromIp } from "./getGeoFromIp";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     Use this to run a local development environment of the library for testing
-  </React.StrictMode>
+    <br></br>
+    {await getGeoFromIp("112.238.182.153")}
+  </React.StrictMode>,
 );
