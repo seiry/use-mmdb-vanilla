433a5dcbd1e50a8bc99f1afc61bf83bd5713e52b
Seiry Yu <4397354+seiry@users.noreply.github.com>
2026-01-01 19:21:13 +1100

    fix: update mmdb.js dependency to version 2.0.3 and refactor main.tsx for improved IP geolocation handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,38 @@
 import { getGeoFromIp } from "./getGeoFromIp";
-import React from "react";
+import React, { FC } from "react";
 import ReactDOM from "react-dom/client";
 
+const ipList = [
+  "112.238.182.153",
+  "142.250.76.46",
+  "999.7.7.7",
+  "2001:41d0:701:1100::29c8",
+  "2404:6800:400a:804::200e",
+  "2404:6800:400a:804::200e99",
+  "772404:6800:400a:804::200e99"
+
+]
+
+const IpTag:FC<{ip:string}> = ({ip})=>{
+  const [geo, setGeo] = React.useState<any>('Loading...');
+
+  React.useEffect(() => {
+    getGeoFromIp(ip).then(setGeo);
+  }, [ip]);
+
+  return (
+    <div>
+      {ip}: {JSON.stringify(geo)}
+    </div>
+  );
+}
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    Use this to run a local development environment of the library for testing
     <br></br>
-    {await getGeoFromIp("112.238.182.153")}
+    {
+      ipList.map((ip) => (
+        <IpTag key={ip} ip={ip} />
+      ))
+    }
   </React.StrictMode>,
 );
